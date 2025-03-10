f3990351db5bb3df2afcffb52a05edb7a4fe7eb1
Samson Bankole <tsbankole@mix.wvu.edu>
2025-03-10 09:08:28 -0500

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+sentences = [
+    "Samson enjoys using apps to earn points. ",
+    "Fetch allows users to earn points which can be redeemed for products.",
+    "Consumers in London hate and dislike reward points and would never use them",
+    "This assignment is completed on March 9th 2025"
+]
